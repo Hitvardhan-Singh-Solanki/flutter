c7d49a476cab224690ec939c4c2262dc4597d8b3
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-04-30 02:03:43 +0530

    creating cards and adding on user interaction

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,51 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
 }
 
-class MyApp extends StatelessWidget {
-  build(context) {
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Easy List"),
-        ),
-      ),
+          appBar: AppBar(
+            title: Text("Easy List"),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text('Add Product'),
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advanced Food Tester');
+                    });
+                  },
+                ),
+              ),
+              Column(
+                children: _products
+                    .map((ele) => Card(
+                          child: Column(
+                            children: <Widget>[
+                              Image.asset('assets/food.jpg'),
+                              Text(ele)
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              )
+            ],
+          )),
     );
   }
 }
