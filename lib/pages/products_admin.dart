4b9dca4d1ff85c4ff81b70d5b03a39c884420d66
hitvardhansolanki <43982595+hitvardhasolanki@users.noreply.github.com>
2019-04-30 04:39:18 +0530

    adding tab navigation

--- lib/pages/products_admin.dart
+++ lib/pages/products_admin.dart
@@ -1,34 +1,52 @@
 import 'package:flutter/material.dart';
 import 'products.dart';
+import 'product_create.dart';
+import 'product_list.dart';
 
 class ProductsAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: Column(
-          children: <Widget>[
-            AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
-            ListTile(
-              title: Text('All Produts'),
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (BuildContext context) => ProductsPage(),
-                  ),
-                );
-              },
-            )
-          ],
+    return DefaultTabController(
+      child: Scaffold(
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              AppBar(automaticallyImplyLeading: false, title: Text('Choose')),
+              ListTile(
+                title: Text('All Produts'),
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (BuildContext context) => ProductsPage(),
+                    ),
+                  );
+                },
+              )
+            ],
+          ),
         ),
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                text: 'Create Product',
+                icon: Icon(Icons.create),
+              ),
+              Tab(
+                text: 'My Products',
+                icon: Icon(Icons.list),
+              )
+            ],
+          ),
+          title: Text('Manage Products'),
+        ),
+        body: TabBarView(children: <Widget>[
+          ProductCreatePage(),
+          ProductListPage()
+        ],),
       ),
-      appBar: AppBar(
-        title: Text('Manage Products'),
-      ),
-      body: Center(
-        child: Text('Manage Your Products'),
-      ),
+      length: 2,
     );
   }
 }
